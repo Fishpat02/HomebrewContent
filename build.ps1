e833e60f31bcdffe851c9ae3be74b83545414ddb
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-02-17 17:40:14 -0500

    Updated weapon details

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-Get-ChildItem -File -Filter *.y*ml | ForEach-Object { $_ | Get-Content | ConvertFrom-Yaml -Ordered | ConvertTo-Json -Depth 10 | Out-File -FilePath ("{0}.json" -f $_.BaseName) }
+Get-ChildItem -File -Filter *.y*ml | ForEach-Object { $_ | Get-Content | ConvertFrom-Yaml -Ordered | ConvertTo-Json -Depth 15 | Out-File -FilePath ("{0}.json" -f $_.BaseName) }
